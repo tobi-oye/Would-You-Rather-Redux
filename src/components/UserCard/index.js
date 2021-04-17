b2921d170ccb16915e3dfe1c0b091b6324c0ac6b
tobi-oye <tobiloba5000@gmail.com>
2021-04-17 21:16:34 +0100

    [bug] - undefined question id bug

--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -1,20 +1,25 @@
 import { connect } from "react-redux";
+import { Redirect } from "react-router";
 import QuestionCard from "../QuestionCard";
 import ResultCard from "../ResultCard";
 
-const UserCard = ({ match, authedUser, checkQuestionId }) => {
+const UserCard = ({ match, authedUser, checkQuestionId, questions }) => {
   return (
     <>
-      {authedUser && checkQuestionId(match.params.id) ? (
-        <ResultCard id={match.params.id} />
+      {questions[match.params.id] ? (
+        authedUser && checkQuestionId(match.params.id) ? (
+          <ResultCard id={match.params.id} />
+        ) : (
+          <QuestionCard id={match.params.id} />
+        )
       ) : (
-        <QuestionCard id={match.params.id} />
+        <Redirect to="/" />
       )}
     </>
   );
 };
 
-const mapStateToProps = ({ users, authedUser }) => {
+const mapStateToProps = ({ users, authedUser, questions }) => {
   const checkQuestionId = (id) => {
     return Object.keys(users[authedUser].answers).some(
       (answerId) => answerId === id
@@ -23,6 +28,7 @@ const mapStateToProps = ({ users, authedUser }) => {
   return {
     authedUser,
     checkQuestionId,
+    questions,
   };
 };
 
